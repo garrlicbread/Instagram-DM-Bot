bcde4113049e305ee426d7f3572dfc18cb2e80bb
Sukant Sindhwani <sukantsindhwani@gmail.com>
2020-12-12 13:29:16 +0530

    Update Instagram_DM(Fast).py

--- Instagram DM Bot/Instagram_DM(Fast).py
+++ Instagram DM Bot/Instagram_DM(Fast).py
@@ -1,9 +1,10 @@
-# Coding an Instagram bot DMs your friends effortlessly (Faster version)
+# Instagram bot that DMs your friends effortlessly (Faster version)
 
+# Initializing start time
 import time
-
 start = time.time()
 
+# Importing libraries
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Defining a function that follows the steps
 class DMBot:
     def __init__(self, user, pw, name, text1, text2):
         
@@ -59,15 +61,18 @@ class DMBot:
         test.send_keys(text2)
         test.send_keys(Keys.ENTER)
 
-time.sleep(1)
-
-IG_username = "kindaahungry" # Enter username
-Password = "austinmartindvg9" # Enter password
-Person_to_Dm = "Poorva Nangia" # Enter follower who you want to text 
-text1 = ("**Hello, the following dm was sent using a (slightly faster) python bot made by Sukant Sindhwani. He wishes you a good day! This bot completes its task in ~20 seconds. :) **")
-text2 = "Hello Bubble. How r u ?" # Enter the message 
-
-DMBot(IG_username, Password, Person_to_Dm, text1, text2) # Calling the class 
-
+# Initializing ending time
 end = time.time()
-print(f"Runtime of the program is {round((end - start), 2)}")
+
+# Entering credentials
+IG_username = "" # Enter username
+Password = "" # Enter password
+Person_to_Dm = str(input("Enter name of recipient: ")) # Enter follower who you want to text 
+text1 = (f"**Hello, the following dm was sent using a (slightly faster) python bot made by Sukant Sindhwani. He wishes you a good day! This bot completes its task in ~20 seconds. :) **")
+text2 = str(input("Enter message: ")) # Enter the message 
+
+# Calling the class 
+DMBot(IG_username, Password, Person_to_Dm, text1, text2) 
+
+# Printing execution time
+print(f"Runtime of the program is {round((end - start), 2)} seconds.")
